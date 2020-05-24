2d7864f8471b1a60d5843440288d224b3ffa41a7
Geoffrey Crofte <geoffrey@MacBookPersodeStephanie.fritz.box>
2020-05-24 13:57:12 +0200

    Cleanup default options

--- inc/admin/register-settings.php
+++ inc/admin/register-settings.php
@@ -11,32 +11,15 @@ function juiz_sps_activation() {
 
 	if ( ! is_array( $juiz_sps_options ) ) {
 		
-		$default_array = array(
-			'juiz_sps_version' 			=> JUIZ_SPS_VERSION,
+		$default_options = array(
+			'juiz_sps_version' 			=> JUIZ_SPS_VERSION, // since 1.4.1
 			'juiz_sps_style' 			=> 7,
-			'juiz_sps_networks' 		=> array(
-				'delicious'		=>	array( 0, __( 'Delicious', 'juiz-social-post-sharer' ) ), // new 1.4.1
-				'digg'	 		=>	array( 0, __( 'Digg', 'juiz-social-post-sharer' ) ),
-				'facebook'		=>	array( 1, __( 'Facebook', 'juiz-social-post-sharer' ) ), 
-				'google'		=>	array( 0, __( 'Google+', 'juiz-social-post-sharer' ) ),
-				'linkedin' 		=>	array( 0, __( 'LinkedIn', 'juiz-social-post-sharer' ) ),
-				'pinterest' 	=>	array( 0, __( 'Pinterest', 'juiz-social-post-sharer' ) ),
-				'reddit'		=>	array( 0, __( 'Reddit', 'juiz-social-post-sharer' ) ), // new 1.4.1
-				'stumbleupon'	=>	array( 0, __( 'StumbleUpon', 'juiz-social-post-sharer' ) ),
-				'twitter'		=>	array( 1, __( 'Twitter', 'juiz-social-post-sharer' ) ), 
-				'tumblr'		=>	array( 0, __( 'Tumblr', 'juiz-social-post-sharer' ) ), // new 1.4.1
-				'viadeo' 		=>	array( 0, __( 'Viadeo', 'juiz-social-post-sharer' ) ),
-				'vk'			=>	array( 0, __( 'VKontakte', 'juiz-social-post-sharer' ) ), // new 1.3.0
-				'weibo'			=>	array( 0, __( 'Weibo', 'juiz-social-post-sharer' ) ), // new 1.2.0
-				'mail'			=>	array( 1, __( 'Email', 'juiz-social-post-sharer' ) ),
-				'bookmark'		=>	array( 0, __( 'Bookmark', 'juiz-social-post-sharer' ) ), // new 1.4.2
-				'print'			=>	array( 0, __( 'Print', 'juiz-social-post-sharer' ) ) // new 1.4.2
-										),
-			'juiz_sps_order'			=> array(), // new 1.5.0
+			'juiz_sps_networks' 		=> array(), // empty since 2.0.0 (see register-networks.php)
+			'juiz_sps_order'			=> array(), // since 2.0.0
 			'juiz_sps_counter'			=> 0,
-			'juiz_sps_counter_option'	=> 'both', // new 1.3.3.7
+			'juiz_sps_counter_option'	=> 'both', // since 1.3.3.7
 			'juiz_sps_hide_social_name' => 0,
-			'juiz_sps_target_link'		=> 0, // new 1.1.0
+			'juiz_sps_target_link'		=> 0, // since 1.1.0
 			'juiz_sps_twitter_user'		=> 'CreativeJuiz',
 			'juiz_sps_display_in_types' => array( 'post' ),
 			'juiz_sps_display_where'	=> 'bottom',
@@ -50,60 +33,15 @@ function juiz_sps_activation() {
 				)
 		);
 		
-		jsps_update_option( $default_array );
+		jsps_update_option( $default_options );
 	}
 	else {
-		// if was version under 1.2.3
-		if ( ! isset( $juiz_sps_options['juiz_sps_force_pinterest_snif'] ) ) {
-			$new_options = array (
-				'juiz_sps_force_pinterest_snif' => 0
-			);
+		// If was version under 2.0.0
+		if ( ! isset( $juiz_sps_options['juiz_sps_version'] ) || ( isset( $juiz_sps_options['juiz_sps_version'] ) && version_compare( $juiz_sps_options['juiz_sps_version'], '2.0.0', '<') ) ) {
 
-			$updated_array = array_merge( $juiz_sps_options, $new_options );
-			jsps_update_option( $updated_array );
-		}
-
-		// if was version under 1.3.0
-		if ( ! isset( $juiz_sps_options['juiz_sps_networks']['vk'] ) ) {
-
-			$juiz_sps_options['juiz_sps_networks']['vk'] = array( 0, __( 'VKontakte', 'juiz-social-post-sharer' ) );
-			$juiz_sps_options['juiz_sps_colors'] = array( 'bg_color' => '', 'txt_color' => ''); // for next update
-
-			jsps_update_option( $juiz_sps_options );
-		}
-
-		// if was version under 1.3.3.7
-		if ( ! isset( $juiz_sps_options['juiz_sps_counter_option'] ) ) {
-
-			$juiz_sps_options['juiz_sps_counter_option'] = 'both';
-
-			jsps_update_option( $juiz_sps_options );
-		}
-
-		// if was version under 1.4.1
-		if ( ! isset( $juiz_sps_options['juiz_sps_version'] ) ) {
+			$juiz_sps_options['juiz_sps_version'] = JUIZ_SPS_VERSION; // since 1.4.1
+			$juiz_sps_options['juiz_sps_order'] = array(); // since 2.0.0
 			
-			$juiz_sps_options['juiz_sps_version'] = JUIZ_SPS_VERSION;
-			$juiz_sps_options['juiz_sps_networks']['tumblr'] 	= array( 0, __( 'Tumblr', 'juiz-social-post-sharer' ) );
-			$juiz_sps_options['juiz_sps_networks']['delicious'] = array( 0, __( 'Delicious', 'juiz-social-post-sharer' ) );
-			$juiz_sps_options['juiz_sps_networks']['reddit'] 	= array( 0, __( 'Reddit', 'juiz-social-post-sharer' ) );
-
-			jsps_update_option( $juiz_sps_options );
-		}
-
-		// if was version under 1.4.2
-		if ( ! isset( $juiz_sps_options['juiz_sps_version'] ) || ( isset( $juiz_sps_options['juiz_sps_version'] ) && version_compare( $juiz_sps_options['juiz_sps_version'], '1.4.2', '<') ) ) {
-			
-			$juiz_sps_options['juiz_sps_version'] = JUIZ_SPS_VERSION;
-			$juiz_sps_options['juiz_sps_networks']['bookmark'] 	= array( 0, __( 'Bookmark', 'juiz-social-post-sharer' ) );
-			$juiz_sps_options['juiz_sps_networks']['print'] = array( 0, __( 'Print', 'juiz-social-post-sharer' ) );
-
-			jsps_update_option( $juiz_sps_options );
-		}
-
-		// if was version under 1.5.0
-		if ( ! isset( $juiz_sps_options['juiz_sps_version'] ) || ( isset( $juiz_sps_options['juiz_sps_version'] ) && version_compare( $juiz_sps_options['juiz_sps_version'], '1.5.0', '<') ) ) {
-			$juiz_sps_options['juiz_sps_order'] = array();
 			jsps_update_option( $juiz_sps_options );
 		}
 	}
