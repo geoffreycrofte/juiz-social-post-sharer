d349aa9c8da9bef3bc235812deba7655587f5a3c
Geoffrey Crofte <452767+geoffreycrofte@users.noreply.github.com>
2020-10-27 19:39:02 +0100

    Mailchimp Campaign tag

--- inc/admin/settings.php
+++ inc/admin/settings.php
@@ -214,16 +214,12 @@ function juiz_sps_setting_radio_style_choice() {
 						<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" form="mc-embedded-subscribe-form">
 					</div>
 
-					<div id="mce-responses" class="clear">
-						<div class="response" id="mce-error-response" style="display:none"></div>
-						<div class="response" id="mce-success-response" style="display:none"></div>
-					</div>
-
 					<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 					<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_5339b8dfa2b000a82251effc3_c9c0f762f1" tabindex="-1" value=""></div>
 
 					<div class="mc-submit">
-						<input type="hidden" value="Admin-Skin-Shop" name="CAMPAIGN" id="mce-CAMPAIGN">
+						<input type="hidden" value="Admin-Skin-Shop" name="CAMPAIGN" id="mce-CAMPAIGN" form="mc-embedded-subscribe-form">
+						
 						<button form="mc-embedded-subscribe-form" type="submit" name="subscribe" id="mc-embedded-subscribe" class="button"><img src="<?php echo JUIZ_SPS_PLUGIN_ASSETS; ?>img/icon-send.svg" alt="Subscribe" title="Subscribe" width="24" height="24" /></button>
 					</div>
 				</div>
