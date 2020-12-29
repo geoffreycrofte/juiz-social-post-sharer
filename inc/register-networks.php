2881a2112efcf8c704ce4ea5500a76dedf3c479c
Geoffrey Crofte <452767+geoffreycrofte@users.noreply.github.com>
2020-12-29 22:57:21 +0100

    New translations in FR plus button name context

--- inc/register-networks.php
+++ inc/register-networks.php
@@ -15,77 +15,77 @@ function jsps_register_core_networks( $core_networks ) {
 
 	$core_networks = array(
 		'diigo'    => array(
-			'name'    => __( 'Diigo', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'Diigo', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 0,
 		),
 		'evernote'    => array(
-			'name'    => __( 'Evernote', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'Evernote', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 0,
 		),
 		'facebook'    => array(
-			'name'    => __( 'Facebook', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'Facebook', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 1,
 		),
 		'linkedin'    => array(
-			'name'    => __( 'LinkedIn', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'LinkedIn', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 0,
 		),
 		'mix'    => array(
-			'name'    => __( 'Mix', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'Mix', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 0,
 		),
 		'pinterest'   => array(
-			'name'    => __( 'Pinterest', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'Pinterest', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 0,
 		),
 		'pocket'   => array(
-			'name'    => __( 'Pocket', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'Pocket', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 0,
 		),
 		'reddit'      => array(
-			'name'    => __( 'Reddit', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'Reddit', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 0,
 		),
 		'twitter'     => array(
-			'name'    => __( 'Twitter', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'Twitter', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 1,
 		),
 		'tumblr'      => array(
-			'name'    => __( 'Tumblr', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'Tumblr', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 0,
 		),
 		'viadeo'      => array(
-			'name'    => __( 'Viadeo', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'Viadeo', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 0,
 		),
 		'vk'          => array(
-			'name'    => __( 'VKontakte', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'VKontakte', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 0,
 		),
 		'weibo'       => array(
-			'name'    => __( 'Weibo', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'Weibo', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 0,
 		),
 		'whatsapp'       => array(
-			'name'    => __( 'WhatsApp', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'WhatsApp', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 0,
 		),
 
 		// Some utilities
 		'shareapi'    => array(
-			'name'    => __( 'Share…', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'Share…', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 1,
 		),
 		'mail'        => array(
-			'name'    => __( 'Email', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'Email', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 1,
 		),
 		'bookmark'    => array(
-			'name'    => __( 'Bookmark', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'Bookmark', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 0,
 		),
 		'print'       => array(
-			'name'    => __( 'Print', 'juiz-social-post-sharer' ),
+			'name'    => _x( 'Print', 'Button Name', 'juiz-social-post-sharer' ),
 			'visible' => 0,
 		),
 	);
